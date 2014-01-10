1b5a3e4f6283e28388216ea20b3d676d64375cd9
ramkumar <sgramkumar@yahoo.com>
2014-01-10 09:59:48 -0800

    modified pom.xml and added testng.xml

---
+++ src/main/java/com/vmware/ramkumar/App.java
@@ -0,0 +1,13 @@
+package com.vmware.ramkumar;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
