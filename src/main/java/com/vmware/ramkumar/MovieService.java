9ccb3df9e1c1449d789ac4c2efe5ef673c423b81
ramkumar <sgramkumar@yahoo.com>
2014-01-10 16:58:15 -0800

    working code again

--- src/main/java/com/vmware/ramkumar/MovieService.java
+++ src/main/java/com/vmware/ramkumar/MovieService.java
@@ -34,33 +34,5 @@ public class MovieService {
 			}
 		}
 		throw new MovieException("That movie does not exist in the service");
-
-
-		// Using Set/HashSet
-		/*
-	private Set<String> movies = new HashSet<String>(); //NEW!
-
-	public MovieService(String title) {
-	}
-
-	public void addMovie(Movie movie) throws MovieException {
-		if (movies.contains(movie.getName()))
-			throw new MovieException("Movie already exists");
-		movies.add(movie.getName()); //NEW!
-	}
-
-	public int getCount() {
-		return movies.size();
-	}
-
-	public void removeByName(String name) throws MovieException {
-		if (getCount() == 0)
-			throw new MovieException("There are no movies in the service");
-
-		if (!movies.remove(name)) {
-			throw new MovieException("That movie does not exist in the service");        
-		}
-	}
-		 */
 	}
 }
