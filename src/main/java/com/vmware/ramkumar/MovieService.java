ffaf6ffe9194608599701198a62708c78c0c667e
ramkumar <sgramkumar@yahoo.com>
2014-01-10 16:50:58 -0800

    all tests and code working

--- src/main/java/com/vmware/ramkumar/MovieService.java
+++ src/main/java/com/vmware/ramkumar/MovieService.java
@@ -1,9 +1,13 @@
 package com.vmware.ramkumar;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class MovieService {
+
+	// Using List/ArrayList
 
 	private List<Movie> movies = new ArrayList<Movie>();
 
@@ -31,6 +35,32 @@ public class MovieService {
 		}
 		throw new MovieException("That movie does not exist in the service");
 
+
+		// Using Set/HashSet
+		/*
+	private Set<String> movies = new HashSet<String>(); //NEW!
+
+	public MovieService(String title) {
 	}
 
+	public void addMovie(Movie movie) throws MovieException {
+		if (movies.contains(movie.getName()))
+			throw new MovieException("Movie already exists");
+		movies.add(movie.getName()); //NEW!
+	}
+
+	public int getCount() {
+		return movies.size();
+	}
+
+	public void removeByName(String name) throws MovieException {
+		if (getCount() == 0)
+			throw new MovieException("There are no movies in the service");
+
+		if (!movies.remove(name)) {
+			throw new MovieException("That movie does not exist in the service");        
+		}
+	}
+		 */
+	}
 }
